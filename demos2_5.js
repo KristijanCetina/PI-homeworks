bf0b2a71da338af2a20ba296a0b14dd7e6bdd2bf
Kristijan Cetina <kristijan.cetina@gmail.com>
2020-11-20 18:07:55 +0100

    Create demos2_5.js

---
+++ demos2_5.js
@@ -0,0 +1,7 @@
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+console.log(validateEmail("tstArcic@studentunipu.hr"))
+console.log(validateEmail("xkn37138@eo.opycom"))
